24ed6064e28f4f15a34cdd8ba5a6045f6dc4ff09
Tayrangel <tayhanarangel@gmail.com>
2022-01-21 18:41:58 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbarhome from './components/Navbarhome';
+import Navbarhome from './components/NavbarHome';
+import BodyHome from './components/BodyHome';
 
 function App() {
   return (
     <div className="App">
       <Navbarhome />
+      <h1>Testando para saber como o degrade irá ficar</h1>
+      <BodyHome />
     </div>
   );
 }
---
+++ src/components/BodyHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+
+export default (props) => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col xs="6"> Testando </Col>
+                    <Col xs="6"> Testando </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
